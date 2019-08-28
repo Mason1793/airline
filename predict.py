e85498ee7b1dd6d80a46f36061497e0ebd165049
Mason_z <375359554@qq.com>
2019-08-28 17:47:06 +0800

    aa

--- predict.py
+++ predict.py
@@ -3,13 +3,13 @@ import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import xlwt
-import excel as xl
+import process_data 
 
 
 
 denominator = 24*60*60
 
-def ratio_accumulate_traveller(interval_time):   
+def ratio_accumulate_traveller_callback(interval_time):   
     temp_x=(math.log(interval_time)-mu)/(sigma*pow(2,0.5))
     p=0.5 - 0.5*math.erf(temp_x);
     return p
@@ -18,20 +18,26 @@ def ratio_accumulate_traveller(interval_time):
 
 
 # 这里修改航班时间
-t0=datetime.datetime.strptime("2019-04-03 21:20:00","%Y-%m-%d %H:%M:%S")
+def ratio_accumulate_traveller(take_off_time) :
+    t0=datetime.datetime.strptime(take_off_time,"%Y-%m-%d %H:%M:%S")
 
-t0_zero=t0-datetime.timedelta(hours=t0.hour,minutes=t0.minute,seconds=t0.second)
-print("t0相对于的0点时间",(t0-t0_zero).seconds)
-t0=(t0-t0_zero).seconds/denominator
-print("t0 转换到0-1空间:",t0);
-mu=43.68*math.pow(t0,4)-95.04*math.pow(t0,3)+71.64*math.pow(t0,2)-21.29*t0-0.7403
-sigma=2.44*math.pow(t0,3)-4.738*math.pow(t0,2)+3.179*t0-0.383
-print("mu:",mu)
-print("sigma:",sigma)
+    t0_zero=t0-datetime.timedelta(hours=t0.hour,minutes=t0.minute,seconds=t0.second)
+    print("t0相对于的0点时间",(t0-t0_zero).seconds)
+    t0=(t0-t0_zero).seconds/denominator
+    print("t0 转换到0-1空间:",t0);
+    mu=43.68*math.pow(t0,4)-95.04*math.pow(t0,3)+71.64*math.pow(t0,2)-21.29*t0-0.7403
+    sigma=2.44*math.pow(t0,3)-4.738*math.pow(t0,2)+3.179*t0-0.383
+    print("mu:",mu)
+    print("sigma:",sigma)
+    temp_x=(math.log(interval_time)-mu)/(sigma*pow(2,0.5))
+    p=0.5 - 0.5*math.erf(temp_x);
+    return p
+
+
 
 # X表示等待时间 ，等待时间为0无意义（ln0），需要设置一个极限值
 X=np.linspace(0.00000001,0.2,100)
-Y=list(map(lambda x: ratio_accumulate_traveller(x),X))
+Y=list(map(lambda x: ratio_accumulate_traveller_callback(x),X))
 plt.plot(X,Y)
 plt.show()
 
