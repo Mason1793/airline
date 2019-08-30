e88151a7728483721628cf8779f769d490f0de27
Mason_z <375359554@qq.com>
2019-08-30 09:20:53 +0800

    plt_ratio

--- predict.py
+++ predict.py
@@ -85,10 +85,12 @@ def count_interval_travellers(moments_to_zero,time_flys_to_zero,travellers_num):
     # plt.bar(range(len(l)), l)
     # every_moment=
     plt.plot(every_moment[1:-1],l[:-1])
-    kd=[]
+    
     # for i in range(len(every_moment[1:-1])):
     #     kd.append(i+1)
+    plt.title('2019-01-16')
     plt.xticks(every_moment[1:-1],rotation=90)
+   
     plt.show()
             
 
@@ -121,11 +123,22 @@ def count_model(data):
     moments_to_zero,time_flys_to_zero = count_interval_time(time_flys)                  ##将时间转换为0-1空间上
     count_interval_travellers(moments_to_zero,time_flys_to_zero,travellers_num)         ##计算间隔人数
 
+# 累计比例图，入参时间转换到0-1空间
+def plt_ratio(fly_time):
+    X=np.linspace(0.00000001,0.2,100)
+    Y=list(map(lambda x: ratio_accumulate_traveller(fly_time,x),X))
+    plt.plot(X,Y)
+    plt.show()
     
 if __name__ == '__main__':
     data = get_data_on_date("/Users/mason/Desktop/补全航班数据.xls","2019-1-16")
-    # print(data)
+    # # print(data)
+    # plt_ratio(0.5)
     count_model(data)
+
+
+    
+
     # p = ratio_accumulate_traveller(0.24333,0.00001)
     # print(p)
     # print("时间格式YYYY-MM-DD hh-mm-ss,如2019-09-01 19:11:11")
@@ -144,10 +157,7 @@ if __name__ == '__main__':
 
 
 # X表示等待时间 ，等待时间为0无意义（ln0），需要设置一个极限值
-# X=np.linspace(0.00000001,0.2,100)
-# Y=list(map(lambda x: ratio_accumulate_traveller(x),X))
-# plt.plot(X,Y)
-# plt.show()
+
 
 
 #2019-04-03 08:00:00
